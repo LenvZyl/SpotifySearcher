a4ea118998ed9f73d60edd6942d2900a03e2693b
Lenx <lenvzyl@gmail.com>
2021-08-03 18:48:28 +0200

    Adding in Spotify SDK

---
+++ SpotifySearch/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  SpotifySearch
+//
+//  Created by Len van Zyl on 2021/08/03.
+//
+
+import Foundation 
+
+struct Constants {
+    static let clientId = "85cb7f0093564f5b8e15e2fe02680790"
+    static let clientSecret = "9f3fb3bd084e49fa900310118208e0aa"
+    static let redirectUrl = URL(string: "spotifysearch://spotify/callback")!
+}
---
+++ SpotifySearch/SpotifySearch-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  SpotifySearch-Bridging-Header.h
+//  SpotifySearch
+//
+//  Created by Len van Zyl on 2021/08/03.
+//
+#ifndef SpotifySearch_Bridging_Header_h
+#define SpotifySearch_Bridging_Header_h
+#import <SpotifyiOS/SpotifyiOS.h>
+
+
+#endif /* SpotifySearch_Bridging_Header_h */
